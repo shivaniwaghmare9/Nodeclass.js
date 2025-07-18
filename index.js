413f06fdea922fca6ec863d3ef62086e503c57ff
shivani <shivaniw69@gmail.com>
2025-07-18 23:13:04 +0530

    index

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 
 const express=require("express");
-app.get()
+const app=express();
+app.get("/", (req,res)=>{
+    res.send("<h1>Welcome to my root page!!</h1>")
+})
+app.listen(4000,()=>{
+    console.log("server is running on port 4000")
+})
+
+ 
