751d1660933985de89e80812063eb5079482675c
shivani <shivaniw69@gmail.com>
2025-08-02 00:50:54 +0530

    node

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
 
 const express=require("express");
 const app=express();
-const dataRoute=require("./routes/dataRoute")
-const mongoose=require("mongoose");
-const bodyparser=require("body-parser");
-mongoose.connect("mongodb://127.0.0.1:27017/shivdb").then(()=>{
-    console.log("Database connected successfully");
+
+
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000 ")
 })
-app.set("view engine","ejs")
-app.use("/",dataRoute)
-
-//Body parser middleware
-app.use(bodyparser.urlencoded({extended:true}));
-app.use(bodyparser.json());
-
-
-app.listen(4000,()=>{
-    console.log("server is running on port 4000")
-})
-
- 
