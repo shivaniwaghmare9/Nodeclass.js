7912c1d4eb683ef46f097bca084a903592ea01ce
shivani <shivaniw69@gmail.com>
2025-08-01 15:25:20 +0530

    controller

--- routes/dataRoute.js
+++ routes/dataRoute.js
@@ -1,7 +1,9 @@
 
 const express=require("express");
 const route=express.Router();
+const dataController=require("../controller/dataController");
 
+route.get("/",dataController.homePage);
 
 
 module.exports=route;
