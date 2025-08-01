0dddbac33c56fe2838d5584197737bc6314e36d4
shivani <shivaniw69@gmail.com>
2025-08-01 15:41:25 +0530

    controller

--- controller/dataController.js
+++ controller/dataController.js
@@ -1,9 +1,13 @@
 
+const DataModel=require("../models/dataModel")
 const homePage=(req,res)=>{
     res.render("home")
 }
-
+const insertPage=(req,res)=>{
+    res.render("insert");
+}
 
 module.exports={
-    homePage
+    homePage,
+    insertPage
 }
