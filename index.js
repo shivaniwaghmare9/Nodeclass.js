c6bde580925b85eede264dfb654cd012c5f2526b
shivani <shivaniw69@gmail.com>
2025-08-02 01:19:56 +0530

    body

--- index.js
+++ index.js
@@ -2,6 +2,16 @@
 const express=require("express");
 const app=express();
 const dataRoute=require("./routes/dataRoute");
+const mongoose=require("mongoose");
+const bodyparser=require("body-parser");
+
+mongoose.connect("mongodb://127.0.0.1:27017/shivdb").then(()=>{
+    console.log("Databse connected successfully")
+})
+
+//Body parser middleware
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.json());
 
 app.set("view engine","ejs")
 app.use("/",dataRoute);
