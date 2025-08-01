459f841d18036be2a5fbb49675e437cb1ea1f1a6
shivani <shivaniw69@gmail.com>
2025-08-02 01:10:14 +0530

    route

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -3,7 +3,11 @@ const dataModel=require("../models/dataModel")
 const homepage=(req,res)=>{
     res.render("home");
 }
+const insertpage=(req,res)=>{
+    res.render("insert");
+}
 
 module.exports={
-    homepage
+    homepage,
+    insertpage
 }
--- routes/dataRoute.js
+++ routes/dataRoute.js
@@ -3,5 +3,7 @@ const express=require("express");
 const route=express.Router();
 const dataController=require("../controllers/dataController");
 
+route.get("/",dataController.homepage);
+route.get("/insert",dataController.insertpage);
 
 module.exports=route;
