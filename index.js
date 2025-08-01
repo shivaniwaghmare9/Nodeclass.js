38b0525d44ca3c047e4f30bb5205524de13590e1
shivani <shivaniw69@gmail.com>
2025-08-01 15:28:27 +0530

    ejs

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 
 const express=require("express");
 const app=express();
+const dataRoute=require("./routes/dataRoute")
+app.set("view engine","ejs")
+app.use("/",dataRoute)
+
 
 app.listen(4000,()=>{
     console.log("server is running on port 4000")
