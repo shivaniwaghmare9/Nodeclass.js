8a5acb2a86d3fa14371b294cc0e92d84a69ba2ca
shivani <shivaniw69@gmail.com>
2025-08-02 00:11:58 +0530

    route

--- routes/dataRoute.js
+++ routes/dataRoute.js
@@ -5,6 +5,7 @@ const dataController=require("../controller/dataController");
 
 route.get("/",dataController.homePage);
 route.get("/insert",dataController.insertPage);
+route.post("/dataSave",dataController.dataSave);
 
 
 module.exports=route;
