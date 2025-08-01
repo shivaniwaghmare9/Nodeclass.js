1901399bb0d627efad8e0c23032b4a8e2ff843af
shivani <shivaniw69@gmail.com>
2025-08-01 14:53:24 +0530

    route

--- index.js
+++ index.js
@@ -1,24 +1,7 @@
 
 const express=require("express");
 const app=express();
-app.get("/", (req,res)=>{
-    res.send("<h1>Welcome to my root page!!</h1>")
-})
-app.get("/home", (req,res)=>{
-    res.send("<h1>Welcome to my home page!!</h1>")
-})
-app.get("/about", (req,res)=>{
-    res.send("<h1>Welcome to my about page!!</h1>")
-})
-app.get("/service", (req,res)=>{
-    res.send("<h1>Welcome to my service page!!</h1>")
-})
-app.get("/photo", (req,res)=>{
-    res.send("<h1>Welcome to my photo page!!</h1>")
-})
-app.get("/contact", (req,res)=>{
-    res.send("<h1>Welcome to my contact page!!</h1>")
-})
+
 app.listen(4000,()=>{
     console.log("server is running on port 4000")
 })
