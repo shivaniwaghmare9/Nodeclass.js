10df3249e124bbd96429c24af506b3f721073e65
shivani <shivaniw69@gmail.com>
2025-10-12 21:49:22 +0530

    js

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
 
+// const express=require("express");
+// const app=express();
+// const dataRoute=require("./routes/dataRoute");
+// const mongoose=require("mongoose");
+// const bodyparser=require("body-parser");
+
+// mongoose.connect("mongodb://127.0.0.1:27017/shivdb").then(()=>{
+//     console.log("Database connected successfully")
+// })
+
+// //Body parser middleware
+// app.use(bodyparser.urlencoded({extended:true}));
+// app.use(bodyparser.json());
+
+// app.set("view engine","ejs")
+// app.use("/",dataRoute);
+// app.use(express.static("./public"))
+
+// app.listen(3000,()=>{
+//     console.log("Server is running on port 3000 ")
+// })
+
 const express=require("express");
 const app=express();
 const dataRoute=require("./routes/dataRoute");
