b078a0cc92a355bcfb328ba0554fea65b7372e2c
shivani <shivaniw69@gmail.com>
2025-10-12 21:43:41 +0530

    route

--- routes/dataRoute.js
+++ routes/dataRoute.js
@@ -1,4 +1,22 @@
 
+// const express=require("express");
+// const route=express.Router();
+// const dataController=require("../controllers/dataController");
+
+// route.get("/",dataController.homepage);
+// route.get("/insert",dataController.insertpage);
+// route.post("/save",dataController.dataSave);
+// route.get("/display",dataController.displayPage);
+// route.get("/update",dataController.updatePage);
+// route.get("/deletedata",dataController.dataDelete);
+// route.get("/editdata",dataController.editPage);
+// route.post("/editsave",dataController.editData);
+// route.get("/search",dataController.searchPage);
+// route.post("/searchdata",dataController.searchData);
+
+
+// module.exports=route;
+
 const express=require("express");
 const route=express.Router();
 const dataController=require("../controllers/dataController");
